f1e3135c6176d4bd1c387072305b00b79dff8c9e
Demonathenos <36627669+Demonathenos@users.noreply.github.com>
2018-02-19 15:34:43 +0100

    Add files via upload

---
+++ heat2.py
@@ -0,0 +1,49 @@
+import io
+N = input("What is the size of the problem")
+config = io.open("problem.def", 'w')
+i=0
+while i < int(N) + 1:
+  i+=1
+for line in io.open('problem.def.templ', 'r'):
+  line = line.replace('$state.dimension', str(int(N)+1))
+  line = line.replace('$boundarycond.dimension', str(int(N)+1))
+  if "$stateconditions" in line:
+      x = 0
+      line=''
+      while x < int(str(N)):
+          x += 1
+          row = 'state.' + str(int(x))+" "+"string state."+str(int(x))+"\n"
+          config.write(row)
+  if "$boundaryconditions" in line:
+      line=''
+      x = 0
+      while x < int(str(N)):
+          x += 1
+          row = 'boundarycond.' + str(int(x))+" "+"string boundarycond."+str(int(x))+"\n"
+          config.write(row)
+  config.write(line)
+config.close()
+
+config1 = io.open('problem.bounds', 'w')
+for line in io.open('problem.bounds.tmpl', 'r'):
+  line = line.replace('$dimension1', str(int(N)+1))
+  line = line.replace('$dimension2', str(int(N) + 1))
+  if "$statevariablebounds" in line:
+      x = 0
+      line=''
+      while x < (int(str(N))+1):
+          x+= 1
+          row = '0 0 free'+'\n'
+          config1.write(row)
+  if "$initandfinconditions" in line:
+      x = 0
+      line=''
+      while x < (int(str(N))+1):
+          x+= 1
+          row = '1 1 equal'+'\n'
+          config1.write(row)
+  config1.write(line)
+
+config1.close()
+
+
